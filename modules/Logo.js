a357615ec0261ab11ac0c1f63bca9ca8efcb17ed
Conail Stewart <conailstewart@gmail.com>
2016-03-26 15:15:08 -0400

    Add gradient fill to logo as originally intended.
    The gradient fill is implemented in SVG, but the color can be specified
    via CSS under '#g1 stop'.

--- modules/Logo.js
+++ modules/Logo.js
@@ -4,6 +4,12 @@ export default class Logo extends React.Component {
   render() {
     return  <div id="logo">
       <svg viewBox="0 0 43 56">
+        <defs>
+          <linearGradient id="g1" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" style={{ stopOpacity: '0.2'  }} />
+            <stop offset="100%" style={{ stopOpacity: '1' }} />
+          </linearGradient>
+        </defs>
         <g>
           <path d="m 9,0  30,0  4,18 -30,0 z"/>
 	  <path d="m 0,37 43,0 0,-18 -40,0 z"/>
