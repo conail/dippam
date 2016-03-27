ee95db28d74119483b0c7cadfcd380587460297d
Conail Stewart <conailstewart@gmail.com>
2016-03-26 20:40:48 -0400

    Add contact information to app footer.

--- modules/AppFooter.js
+++ modules/AppFooter.js
@@ -4,14 +4,27 @@ export default class AppFooter extends React.Component {
   render() {
     return <footer>
       <div className="sponsors">
-        <h1>Sponsors</h1>
+        <h3>Sponsors</h3>
         <ul>
-	  <li>Queen's University Belfast</li>
-	  <li>The Mellon Centre</li>
-	  <li>Arts and Humanities Research Council</li>
-	  <li>University of Ulster</li>
-	  <li>Libraries NI</li>
+	  <li><a href="http://www.qub.ac.uk/schools/SchoolofHistoryandAnthropology/">Queen's University Belfast</a></li>
+          <li><a href="http://www.qub.ac.uk/cms">The Mellon Centre</a></li>
+	  <li><a href="">Arts and Humanities Research Council</a></li>
+	  <li><a href="http://www.socsci.ulster.ac.uk/policy/index.html">University of Ulster</a></li>
+	  <li><a href="">Libraries NI</li>
         </ul>
+      </div>
+      <div className="contact">
+        <h3>Contact</h3>
+        <p>For more information please send an email to info@dippam.ac.uk, or address mail to:</p>
+        <address>
+          DIPPAM Project,
+          School of History and Anthropology,
+          15 University Square,
+          Queen's University Belfast,
+          Belfast
+          BT7 1NN
+          UK
+        </address>
       </div>
     </footer>;
   }
