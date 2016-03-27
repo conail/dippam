9009f457dceef6cfc3a6711be4f846ef749b0c4e
Conail Stewart <conailstewart@gmail.com>
2016-03-27 10:03:39 -0400

    Add routes for search under each collection.

--- modules/routes.js
+++ modules/routes.js
@@ -7,13 +7,18 @@ import SearchPage from './pages/SearchPage';
 import CollectionPage from './pages/CollectionPage';
 import EthicalUsePolicyPage from './pages/EthicalUsePolicyPage';
 import TeamPage from './pages/TeamPage';
+import DocumentPage from './pages/DocumentPage';
 
 module.exports = <Route path="/" component={App}>
   <IndexRoute component={HomePage}/>
   <Route path="/search" component={SearchPage}/>
+  <Route path="/search/:query" component={SearchPage}/>
   <Route path="/about" component={AboutPage}/>
   <Route path="/collections" component={HomePage}/>
-  <Route path="/collections/:collection" component={SearchPage}/>
+  <Route path="/collections/:collection" component={CollectionPage}/>
+  <Route path="/collections/:collection/search" component={SearchPage}/>
+  <Route path="/collections/:collection/search/:query" component={SearchPage}/>
   <Route path="/policies/ethical-use" component={EthicalUsePolicyPage}/>
   <Route path="/team" component={TeamPage}/>
+  <Route path="/document/:id" component={DocumentPage}/>
 </Route>;
