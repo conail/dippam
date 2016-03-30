c5327f7e054f486f15ac9d73a760229606b903da
Conail Stewart <conailstewart@gmail.com>
2016-03-30 09:21:45 -0400

    First cut of dynamic facet list generation.

--- modules/pages/SearchPage.js
+++ modules/pages/SearchPage.js
@@ -3,108 +3,18 @@ import React from 'react';
 export default class SearchPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = require('json!../../data/collections.json');
+    this.state = { facets: require('json!../../data/facets') };
   }
 
   render() {
-    const documentTypes = [ 
-      {id: '', name: 'Account', collection: ''},
-      {id: '', name: 'Bill', collection: ''},
-      {id: '', name: 'Report', collection: ''},
-      {id: '', name: 'Command Paper', collection: ''},
-      {id: '', name: 'Diaries and Journals', collection: ''},
-      {id: '', name: 'Family Papers', collection: ''},
-      {id: '', name: 'Folklore, Song, Music', collection: ''},
-      {id: '', name: 'Hansard', collection: ''},
-      {id: '', name: 'Letters (Emigrants)', collection: ''},
-      {id: '', name: 'Letters (Other)', collection: ''},
-      {id: '', name: 'Births, Deaths, Marriages', collection: ''},
-      {id: '', name: 'Newspapers Extracts', collection: ''},
-      {id: '', name: 'Shipping Advertisements', collection: ''},
-      {id: '', name: 'Shipping News', collection: ''},
-      {id: '', name: 'Official Documents', collection: ''},
-      {id: '', name: 'Periodical Extracts', collection: ''},
-      {id: '', name: 'Statistics', collection: ''},
-      {id: '', name: 'Wills', collection: ''}
-    ];
-
-    const collections = [
-      {id: '', name: 'Enhanced British Parliamentary Papers on Ireland' },
-      {id: '', name: 'Irish Emigration Database' },
-      {id: '', name: 'Voices of Migration and Return' }
-    ];
-
     return <div className="page" id="search">
       <form id="query">
-        <fieldset>
-          <legend>Search Mode</legend>
-          <select>
-            <option>Full text</option>
-            <option>Titles</option>
-            <option>Exact Title</option>
-            <option>Command Number</option>
-            <option>Paper Number</option>
-         </select>
-       </fieldset>
-       <FacetList title="Collections" items={collections}/>
-       <fieldset>
-         <legend>Time Period</legend>
-         <label>
-           <span>From</span>
-           <input type="date"/>
-         </label>
-         <label>
-           <span>To</span>
-           <input type="date"/>
-         </label>
-         <label>
-           <span>Emigration</span>
-           <input type="date"/>
-         </label>
-         <label>
-           <span>Return</span>
-           <input type="date"/>
-         </label>
-      </fieldset>
-      <FacetList title="Document Types" items={documentTypes}/>
-      <fieldset>
-        <legend>Demographic</legend>
-        <label>
-          <span>Gender</span>
-          <input type="text"/>
-        </label>
-        <label>
-          <span>Age</span>
-          <input type="text"/>
-        </label>
-        <label>
-          <span>Denomination</span>
-          <input type="text"/>
-        </label>
-      </fieldset>
-      <fieldset>
-          <legend>Places</legend>
-          <label>
-            <span>Place</span>
-            <input type="text"/>
-          </label>
-          <label>
-            <span>Birthplace</span>
-            <input type="text"/>
-          </label>
-          <label>
-            <span>Childhood Residence</span>
-            <input type="text"/>
-          </label>
-          <label>
-            <span>Current Residence</span>
-            <input type="text"/>
-          </label>
-          <label>
-            <span>Destination</span>
-            <input type="text"/>
-          </label>
-        </fieldset>
+        <Select title="Search Mode" items={this.state.facets.mode}/>
+        <FacetList title="Collections" items={this.state.facets.db}/>
+        <FacetList title="Time Period" items={this.state.facets.period}/>
+        <FacetList title="Document Types" items={this.state.facets.documentTypes}/>
+        <FacetList title="Demographics" items={this.state.facets.demographics}/>
+        <FacetList title="Places" items={this.state.facets.places}/>
       </form>
       <div id="results">
         <table>
@@ -121,6 +31,22 @@ export default class SearchPage extends React.Component {
         </table>
       </div>
     </div>;
+  }
+}
+
+class Select extends React.Component {
+  render() {
+    return <fieldset>
+      <legend>{this.props.title}</legend>
+      <select name="mode" onChange={this.handleChange}>
+        {this.props.items.map(x => <option key={x.id} {...x}>{x.name}</option>)}
+      </select>
+    </fieldset>;
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.props.handleChange();
   }
 }
 
