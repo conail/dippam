511277d9ea253c0a5f45bee33c78764290ab9a34
Conail Stewart <conailstewart@gmail.com>
2016-04-15 19:19:27 -0400

    ES6 syntax.

--- modules/App.js
+++ modules/App.js
@@ -1,27 +1,16 @@
-import React, {Component} from 'react'
+import React from 'react'
 import NavLink from './NavLink'
 import AppHeader from './AppHeader'
 import AppFooter from './AppFooter'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      search: {
-        query: '',
-        facets: [],
-        results: []
-      }
-    }
-  }
-
-  render() {
-    return <div id="app" className={this.props.params.collection}>
-      <AppHeader {...this.props}/>
-      <main>
-        {this.props.children}
-      </main>
-      <AppFooter/>
-    </div>
-  }
+const App = () => {
+  return <div id="app" className={this.props.params.collection}>
+    <AppHeader {...this.props}/>
+    <main>
+      {this.props.children}
+    </main>
+    <AppFooter/>
+  </div>
 }
+
+export default App
