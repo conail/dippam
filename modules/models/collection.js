e00dfd82518276af6d24dd029609cfa2871c85dd
Conail Stewart <conailstewart@gmail.com>
2016-04-19 17:53:51 -0400

    Correct bad autoindentation.

--- modules/models/collection.js
+++ modules/models/collection.js
@@ -1,61 +1,59 @@
 export default class Collection {
-    constructor(title = '', description = '', uri ='/', from, to) {
-	this.title = title
-	this.description = description
-	this.uri = uri
-	this.from = from || 1700
-	this.to = to || new Date().getFullYear()
-    }
+  constructor(title = '', description = '', uri ='/', from, to) {
+    this.title = title
+    this.description = description
+    this.uri = uri
+    this.from = from || 1700
+    this.to = to || new Date().getFullYear()
+  }
 }
 
 export class IEDCollection extends Collection {
-    constructor() {
-	super();
-	
-	this.types = [
-	    "Diaries and Journals",
-	    "Family Papers",
-	    "Folklore, Song, Music",
-	    "Hansard",
-	    "Letters (Emigrants)",
-	    "Letters (Other)",
-	    "Births, Deaths, Marriages",
-	    "Newspapers Extracts",
-	    "Shipping Advertisements",
-	    "Shipping News",
-	    "Official Documents",
-	    "Periodical Extracts",
-	    "Statistics",
-	    "Wills",
-	    "Interviews"
-	]
-    }
+  constructor() {
+    super()
+    
+    this.types = [
+      "Diaries and Journals",
+      "Family Papers",
+      "Folklore, Song, Music",
+      "Hansard",
+      "Letters (Emigrants)",
+      "Letters (Other)",
+      "Births, Deaths, Marriages",
+      "Newspapers Extracts",
+      "Shipping Advertisements",
+      "Shipping News",
+      "Official Documents",
+      "Periodical Extracts",
+      "Statistics",
+      "Wills"
+    ]
+  }
 }
 
 export class EPPICollection extends Collection {
-    constructor() {
-	super();
+  constructor() {
+    super()
 
-	this.types = ["Account", "Bill", "Report", "Command Paper"]
-    }
+    this.types = ["Account", "Bill", "Report", "Command Paper"]
+  }
 }
 
 export class VMRCollection extends Collection {
-    constructor() {
-	super();
+  constructor() {
+    super()
 
-	this.types = ["Interviews"]
-	
-	// Demographics
-	this.gender = "";
-	this.age = "";
-	this.denomination = "";
-	
-	// Places 
-	this.birthplace = "";
-	this.childhoodResidence = "";
-	this.currentResidence = "";
-	this.destination = "";
-    }
+    this.types = ["Interviews"]
+    
+    // Demographics
+    this.gender = "";
+    this.age = "";
+    this.denomination = "";
+    
+    // Places 
+    this.birthplace = "";
+    this.childhoodResidence = "";
+    this.currentResidence = "";
+    this.destination = "";
+  }
 }
-
