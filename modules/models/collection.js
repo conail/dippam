62601bea68e936067fbc3bd8424d390cb579a550
Conail Stewart <conailstewart@gmail.com>
2016-04-19 22:23:00 +0200

    Add types for subcollections.

--- modules/models/collection.js
+++ modules/models/collection.js
@@ -7,3 +7,55 @@ export default class Collection {
 	this.to = to || new Date().getFullYear()
     }
 }
+
+export class IEDCollection extends Collection {
+    constructor() {
+	super();
+	
+	this.types = [
+	    "Diaries and Journals",
+	    "Family Papers",
+	    "Folklore, Song, Music",
+	    "Hansard",
+	    "Letters (Emigrants)",
+	    "Letters (Other)",
+	    "Births, Deaths, Marriages",
+	    "Newspapers Extracts",
+	    "Shipping Advertisements",
+	    "Shipping News",
+	    "Official Documents",
+	    "Periodical Extracts",
+	    "Statistics",
+	    "Wills",
+	    "Interviews"
+	]
+    }
+}
+
+export class EPPICollection extends Collection {
+    constructor() {
+	super();
+
+	this.types = ["Account", "Bill", "Report", "Command Paper"]
+    }
+}
+
+export class VMRCollection extends Collection {
+    constructor() {
+	super();
+
+	this.types = ["Interviews"]
+	
+	// Demographics
+	this.gender = "";
+	this.age = "";
+	this.denomination = "";
+	
+	// Places 
+	this.birthplace = "";
+	this.childhoodResidence = "";
+	this.currentResidence = "";
+	this.destination = "";
+    }
+}
+
