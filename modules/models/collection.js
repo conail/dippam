919646edd66a8d286d5fa9533db2ea9624dcc644
Conail Stewart <conailstewart@gmail.com>
2016-04-19 22:05:02 +0200

    Add year filter.

--- modules/models/collection.js
+++ modules/models/collection.js
@@ -1,7 +1,9 @@
 export default class Collection {
-    constructor(title = '', description = '', uri ='/') {
+    constructor(title = '', description = '', uri ='/', from, to) {
 	this.title = title
 	this.description = description
 	this.uri = uri
+	this.from = from || 1700
+	this.to = to || new Date().getFullYear()
     }
 }
--- spec/models/collection.spec.js
+++ spec/models/collection.spec.js
@@ -6,4 +6,9 @@ describe("A collection", () => {
     it("contains a title", () => expect(c.title).toBe("a title"))
     it("contains a description", () => expect(c.description).toBe("a description"))
     it("has a default URI prefix.", () => expect(c.uri).toBe("/"))
+
+    describe("filters", () => {
+	it("always has a year range from filter", () => expect(typeof c.from).toBe("number"))
+	it("always has a year range to filter", () => expect(typeof c.to).toBe("number"))
+    })
 })
