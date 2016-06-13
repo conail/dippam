c77291ee2b5bb88af718c798e7f49b665885a384
Conail Stewart <conailstewart@gmail.com>
2016-06-13 12:33:40 +0100

    Pruning.

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, browserHistory } from 'react-router'
-import routes from './modules/routes'
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './modules/routes';
 
 // Force CSS to load.  This should use extract text in production.
-import styles from './sass/App.sass'
-
-const API_BASE = "http://localhost:3000"
-
-//var get = new Promise(function(resolve, reject) {
-//  var xhr = new XMLHttpRequest();
-//  xhr.open('get', `${API_BASE}/collections`);
-//  xhr.send();
-//  xhr.onload = () => (this.status >= 200 && this.status < 300) ?
-//    resolve(this.response) : reject(this.statusText)
-//  xhr.onerror = () => reject(this.statusText)
-//});
+import styles from './sass/App.sass';
 
 render(<Router routes={routes} history={browserHistory}/>,
-       document.querySelector("#app"))
+       document.querySelector("#app"));
 
 
 
