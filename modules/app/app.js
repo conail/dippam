2d69beadaeb2c2e81c0424eca0f109d44ea91ba0
Conail Stewart <conailstewart@gmail.com>
2016-06-16 21:56:28 +0100

    Load collection metadata via API.

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,14 +1,24 @@
 import React, {Component} from "react";
 import Header from "./header";
 import Footer from "./footer";
+import api from "./api";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {collections: []};
+  }
+  
+  componentDidMount() {
+    api.collections().then(x => this.setState(x));
+  }
+  
   render() {
     return <div className={this.props.params.collection}>
-      <Header {...this.props}/>
-        <main>
-          {this.props.children}
-        </main>
+      <Header {...this.props} {...this.state}/>
+      <main>
+        {this.props.children}
+      </main>
       <Footer/>
     </div>
   }
