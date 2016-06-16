b28deaab581366a6af038959b87b7e89681c743d
Conail Stewart <conailstewart@gmail.com>
2016-06-16 21:57:28 +0100

    Refactor to stateless functional components.

--- modules/app/footer.js
+++ modules/app/footer.js
@@ -1,42 +1,38 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router'
+import React, {Component} from 'react';
+import {Link} from 'react-router';
 
-const AppFooter = () => {
-  const year = new Date().getFullYear()
+const AppFooter = (props) => <footer>
+  <div className="sponsors">
+  <h3>Sponsors</h3>
+    <ul>
+      <li><a href="https://www.qub.ac.uk/historyandanthropology/">Queen's University Belfast</a></li>
+      <li><a href="https://www.qub.ac.uk/cms">The Mellon Centre</a></li>
+      <li><a href="http://www.ahrc.ac.uk/">Arts and Humanities Research Council</a></li>
+      <li><a href="http://www.socsci.ulster.ac.uk/policy/">University of Ulster</a></li>
+      <li><a href="https://www.librariesni.org.uk/">Lirbaries NI</a></li>
+    </ul>
+  </div>
+  <div className="contact">
+    <h3>Contact</h3>
+    <p>For more information, please email <a href="mailto:info@dippam.ac.uk">info@dippam.ac.uk</a> or address mail to:</p>
+    <address>
+      DIPPAM Project,
+      School of History and Anthropology,
+      15 University Square,
+      Queen's University Belfast,
+      Belfast
+      BT7 1NN
+      UK
+    </address>
+  </div>
+  <div className="policy">
+    <ul>
+      <li><Link to="/policy/privacy">Privacy Policy</Link></li>
+      <li><Link to="/policy/ethical-use">Ethical Use Policy</Link></li>
+    </ul>
+  </div>
+  <p className="copyright">Copyright &copy; {new Date().getFullYear()} DIPPAM.  All Rights Reserved.</p>
+</footer>;
 
-  return <footer>
-    <div className="sponsors">
-      <h3>Sponsors</h3>
-      <ul>
-        <li><a href="https://www.qub.ac.uk/historyandanthropology/">Queen's University Belfast</a></li>
-        <li><a href="https://www.qub.ac.uk/cms">The Mellon Centre</a></li>
-	<li><a href="http://www.ahrc.ac.uk/">Arts and Humanities Research Council</a></li>
-	<li><a href="http://www.socsci.ulster.ac.uk/policy/">University of Ulster</a></li>
-	<li><a href="https://www.librariesni.org.uk/">Lirbaries NI</a></li>
-      </ul>
-    </div>
-    <div className="contact">
-      <h3>Contact</h3>
-      <p>For more information, please email <a href="mailto:info@dippam.ac.uk">info@dippam.ac.uk</a> or address mail to:</p>
-      <address>
-        DIPPAM Project,
-        School of History and Anthropology,
-        15 University Square,
-        Queen's University Belfast,
-        Belfast
-        BT7 1NN
-        UK
-      </address>
-    </div>
-    <div className="policy">
-      <ul>
-        <li><Link to="/policy/privacy">Privacy Policy</Link></li>
-        <li><Link to="/policy/ethical-use">Ethical Use Policy</Link></li>
-      </ul>
-    </div>
-    <p className="copyright">Copyright &copy; {year} DIPPAM.  All Rights Reserved.</p>
-  </footer>
-}
-
-export default AppFooter
+export default AppFooter;
 
--- modules/app/logo.js
+++ modules/app/logo.js
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-export default class Logo extends React.Component {
-  render() {
-    return  <div id="logo">
-      <svg viewBox="0 0 43 56">
-        <defs>
-          <linearGradient id="g1" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" style={{stopOpacity: 0.2}} />
-            <stop offset="100%" style={{stopOpacity: 1}} />
-          </linearGradient>
-        </defs>
-        <g>
-          <path d="m 9,0  30,0  4,18 -30,0 z"/>
-	  <path d="m 0,37 43,0 0,-18 -40,0 z"/>
-	  <path d="m 8,38 35,0 -4,18 -37,0 z"/>
-        </g>
-      </svg>
-      <h1>{this.props.params.collection || 'DIPPAM'}</h1>
-    </div>
-  }
-}
+const Logo = (props) => <div id="logo">
+  <svg viewBox="0 0 43 56">
+    <defs>
+      <linearGradient id="g1" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" style={{stopOpacity: 0.2}} />
+        <stop offset="100%" style={{stopOpacity: 1}} />
+      </linearGradient>
+    </defs>
+    <g>
+      <path d="m 9,0  30,0  4,18 -30,0 z"/>
+      <path d="m 0,37 43,0 0,-18 -40,0 z"/>
+      <path d="m 8,38 35,0 -4,18 -37,0 z"/>
+    </g>
+  </svg>
+  <h1>{props.params.collection || 'DIPPAM'}</h1>
+</div>
+
+export default Logo;
